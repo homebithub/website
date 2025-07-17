27b4fb2d037aa873209bc0911d553565492df3f1
Sean <nahaseannjenga@gmail.com>
2025-07-17 19:41:20 +0300

    Remove page title from `LocationRoute` component.

--- app/routes/location.tsx
+++ app/routes/location.tsx
@@ -4,7 +4,7 @@ import Location from "../components/Location";
 const LocationRoute = () => {
   return (
     <div style={{ minHeight: "100vh", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", background: "#f7f9fa" }}>
-      <h1 style={{ marginBottom: 32, color: "#1a73e8", fontWeight: 700 }}>Location Search Demo</h1>
+
       <Location onSelect={(location) => {}} />
     </div>
   );
