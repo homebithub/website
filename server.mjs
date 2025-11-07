f23fa60a2812b2f68c4612caa5dadacc22c9ccb5
Nahashon Njenga <nahaseannjenga@gmail.com>
2025-11-07 05:10:59 +0300

    Removed golang server and replaced it with fastify

--- server.mjs
+++ server.mjs
@@ -5,7 +5,7 @@ import fastifyHttpProxy from "@fastify/http-proxy";
 import path from "path";
 import { fileURLToPath } from "url";
 import * as build from "./build/server/index.js";
-import { createRequestHandler } from "@react-router/serve";
+import { createRequestHandler } from "@react-router/express";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
