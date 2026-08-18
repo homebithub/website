c4a465e1da5ef012b96aefb05c5d4978440ec919
Sean <nahaseannjenga@gmail.com>
2026-08-18 21:44:52 +0300

    make signup content area independently scrollable

--- app/routes/signup.tsx
+++ app/routes/signup.tsx
@@ -682,7 +682,7 @@ export default function SignupPage() {
         <div className="min-h-screen flex flex-col">
         <Navigation/>
         <PurpleThemeWrapper variant="light" bubbles={false} bubbleDensity="low" className="flex-1 signup-page">
-        <main className="flex-1 flex min-h-0 flex-col justify-start md:justify-center items-center px-4 py-8">
+        <main className="flex-1 flex min-h-0 w-full flex-col justify-start items-center overflow-y-auto px-4 py-8">
             {/* Profile Selection Modal */}
             <Modal 
                 isOpen={isProfileModalOpen} 
